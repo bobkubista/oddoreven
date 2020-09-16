965ca34c20ccc142ca94802fe5c260d84fcaac98
Bob Kubista <b.kubista@first8.nl>
2020-09-16 13:24:22 +0200

    auto formatting

--- src/main/java/com/bobkubista/oddoreven/OddOrEven.java
+++ src/main/java/com/bobkubista/oddoreven/OddOrEven.java
@@ -4,11 +4,11 @@ import java.util.stream.IntStream;
 
 public class OddOrEven {
 
-    public boolean isEven(int[] numbers) {
-        if (numbers.length == 0) {
-            return true;
-        } else {
-            return IntStream.of(numbers).sum() % 2 == 0;
-        }
-    }
+	public boolean isEven(int[] numbers) {
+		if (numbers.length == 0) {
+			return true;
+		} else {
+			return IntStream.of(numbers).sum() % 2 == 0;
+		}
+	}
 }
--- src/test/java/com/bobkubista/oddoreven/OddOrEvenTest.java
+++ src/test/java/com/bobkubista/oddoreven/OddOrEvenTest.java
@@ -5,50 +5,49 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-
 class OddOrEvenTest {
 
-    private OddOrEven ooe = new OddOrEven();
+	private OddOrEven ooe = new OddOrEven();
 
-    @Test
-    void testEmptyArray() {
-        int[] numbers = {};
-        assertTrue(ooe.isEven(numbers));
-    }
+	@Test
+	void testEmptyArray() {
+		int[] numbers = {};
+		assertTrue(ooe.isEven(numbers));
+	}
 
-    @Test
-    void testEvenSingleValue() {
-        int[] numbers = { 2 };
-        assertTrue(ooe.isEven(numbers));
-    }
+	@Test
+	void testEvenSingleValue() {
+		int[] numbers = { 2 };
+		assertTrue(ooe.isEven(numbers));
+	}
 
-    @Test
-    void testOddSingleValue() {
-        int[] numbers = { 1 };
-        assertFalse(ooe.isEven(numbers));
-    }
+	@Test
+	void testOddSingleValue() {
+		int[] numbers = { 1 };
+		assertFalse(ooe.isEven(numbers));
+	}
 
-    @Test
-    void testEvenDoubleValue() {
-        int[] numbers = { 2, 6 };
-        assertTrue(ooe.isEven(numbers));
-    }
+	@Test
+	void testEvenDoubleValue() {
+		int[] numbers = { 2, 6 };
+		assertTrue(ooe.isEven(numbers));
+	}
 
-    @Test
-    void testOddDoubleValue() {
-        int[] numbers = { 1, 3 };
-        assertTrue(ooe.isEven(numbers));
-    }
+	@Test
+	void testOddDoubleValue() {
+		int[] numbers = { 1, 3 };
+		assertTrue(ooe.isEven(numbers));
+	}
 
-    @Test
-    void testOddAndEvenValues() {
-        int[] numbers = { 1, 2 };
-        assertFalse(ooe.isEven(numbers));
-    }
+	@Test
+	void testOddAndEvenValues() {
+		int[] numbers = { 1, 2 };
+		assertFalse(ooe.isEven(numbers));
+	}
 
-    @Test
-    void testNegativeValues() {
-        int[] numbers = { 0, -1, -5 };
-        assertTrue(ooe.isEven(numbers));
-    }
+	@Test
+	void testNegativeValues() {
+		int[] numbers = { 0, -1, -5 };
+		assertTrue(ooe.isEven(numbers));
+	}
 }
